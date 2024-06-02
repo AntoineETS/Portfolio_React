d1ceead941631fbba7e84d729b8ec45900140b8d
AntoineETS <116309614+AntoineETS@users.noreply.github.com>
2024-06-02 14:47:09 -0400

    Added basic code for pages

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>À propos de nous</h2>
+      <p>Ceci est la page À propos.</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h2>Contactez-nous</h2>
+      <p>Ceci est la page Contact.</p>
+    </div>
+  );
+}
+
+export default Contact;
