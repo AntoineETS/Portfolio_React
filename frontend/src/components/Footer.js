71585b6771e22173fb12f92080ed286dd477cd92
AntoineETS <116309614+AntoineETS@users.noreply.github.com>
2024-06-09 10:26:26 -0400

    Tried supporting different screen sizes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -12,13 +12,13 @@ function Footer({ activeTab, onTabChange }) {
                 <p class="wow fadeInUp" data-wow-delay="0s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>follow me</p> 
                 <ul class="horizontal-list">
                     <li className="item1" id="ln" class="wow fadeInUp" data-wow-delay="0.4s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
-                        <a href="https://www.linkedin.com/in/antoine-lamontagne-989121230/">linkedIn</a>
+                        <a href="https://www.linkedin.com/in/antoine-lamontagne-989121230/">ln</a>
                     </li>
                     <li className="item2" id="ig" class="wow fadeInUp" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
-                        <a href="https://www.instagram.com/antoine.lamontagne/">instagram</a>
+                        <a href="https://www.instagram.com/antoine.lamontagne/">ig</a>
                     </li>
                     <li className="item3" id="fb" class="wow fadeInUp" data-wow-delay="0.8s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
-                        <a href="https://www.facebook.com/antoine.lamontagne.315/">facebook</a>
+                        <a href="https://www.facebook.com/antoine.lamontagne.315/">fb</a>
                     </li>
                     <li className="item4" id="git" class="wow fadeInUp" data-wow-delay="1s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
                         <a href="https://github.com/AntoineETS">git</a>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Bienvenue sur mon site web !</h1>
+            <h1 className='title'>Bienvenue sur mon site web !</h1>
             <p>C'est un gros chantier, mais je travaille activement pour le rendre fonctionnel.</p>
         </div>
     );
