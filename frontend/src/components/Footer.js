a6b9fd71cab3cad9842abfb019647d5c9b011cbd
AntoineETS <116309614+AntoineETS@users.noreply.github.com>
2024-06-09 12:55:14 -0400

    Added item images

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -11,16 +11,16 @@ function Footer({ activeTab, onTabChange }) {
             <div class="info" id="media">
                 <p class="wow fadeInUp" data-wow-delay="0s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>follow me</p> 
                 <ul class="horizontal-list">
-                    <li className="item1" id="ln" class="wow fadeInUp" data-wow-delay="0.4s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                    <li class="item1">
                         <a href="https://www.linkedin.com/in/antoine-lamontagne-989121230/">ln</a>
                     </li>
-                    <li className="item2" id="ig" class="wow fadeInUp" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                    <li class="item2">
                         <a href="https://www.instagram.com/antoine.lamontagne/">ig</a>
                     </li>
-                    <li className="item3" id="fb" class="wow fadeInUp" data-wow-delay="0.8s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                    <li class="item3">
                         <a href="https://www.facebook.com/antoine.lamontagne.315/">fb</a>
                     </li>
-                    <li className="item4" id="git" class="wow fadeInUp" data-wow-delay="1s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                    <li class="item4">
                         <a href="https://github.com/AntoineETS">git</a>
                     </li>
                 </ul>
