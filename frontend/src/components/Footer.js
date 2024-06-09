a843d1bbe40f73db164848c6a097999a4f96498e
AntoineETS <116309614+AntoineETS@users.noreply.github.com>
2024-06-09 09:55:17 -0400

    Ajusted the Footer and added images

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,30 @@ import './../css/Footer.css';
 function Footer({ activeTab, onTabChange }) {
     return (
         <footer className="Footer">
-            <p>Copyrights Antoine Lamontagne.</p>
+
+            <div class='hr'>
+                <div class='row'></div>
+            </div>
+            <div class="info" id="media">
+                <p class="wow fadeInUp" data-wow-delay="0s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>follow me</p> 
+                <ul class="horizontal-list">
+                    <li className="item1" id="ln" class="wow fadeInUp" data-wow-delay="0.4s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                        <a href="https://www.linkedin.com/in/antoine-lamontagne-989121230/">linkedIn</a>
+                    </li>
+                    <li className="item2" id="ig" class="wow fadeInUp" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                        <a href="https://www.instagram.com/antoine.lamontagne/">instagram</a>
+                    </li>
+                    <li className="item3" id="fb" class="wow fadeInUp" data-wow-delay="0.8s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                        <a href="https://www.facebook.com/antoine.lamontagne.315/">facebook</a>
+                    </li>
+                    <li className="item4" id="git" class="wow fadeInUp" data-wow-delay="1s" style={{ visibility: 'visible', animationDelay: '0s', animationName: 'fadeInUp' }}>
+                        <a href="https://github.com/AntoineETS">git</a>
+                    </li>
+                </ul>
+                <br></br>
+                <br></br>
+            </div>
+            <p>antoine.lamontagne.1@ens.etsmtl.ca</p>
         </footer>
     );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,35 +1,29 @@
 import React from 'react';
-import homeImage from './../images/Home.png';
+import './../css/Header.css';
 
 function Header({ activeTab, onTabChange }) {
-    return (
-        <header>
-            <nav>
-                <ul style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', listStyle: 'none', padding: 0 }}>
-                    <li>
-                        <button class="button" onClick={() => onTabChange('home')} className={activeTab === 'home' ? 'active' : ''}>
-                        <img src={homeImage} alt="Bouton home" style={{ width: '40px', height: '28px' }} />
-                        </button>
-                    </li>
-                    <li style={{ margin: '0 auto' }}>
-                        <button class="button" onClick={() => onTabChange('projects')} className={activeTab === 'projects' ? 'active' : ''}>
-                            Projects
-                        </button>
-                    </li>
-                    <li style={{ margin: '0 auto' }}>
-                        <button onClick={() => onTabChange('about')} className={activeTab === 'about' ? 'active' : ''}>
-                            About
-                        </button>
-                    </li>
-                    <li style={{ margin: '0 auto' }}>
-                        <button onClick={() => onTabChange('contact')} className={activeTab === 'contact' ? 'active' : ''}>
-                            Contact
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
-}
+
+  return (
+    <header className="Header">
+      <div style={styles.div} onClick={() => onTabChange('home')} className={activeTab === 'home' ? 'active' : ''}>Home</div>
+      <div style={styles.div} onClick={() => onTabChange('projects')} className={activeTab === 'projects' ? 'active' : ''}>Mes projets</div>
+      <div style={styles.div} onClick={() => onTabChange('about')} className={activeTab === 'about' ? 'active' : ''}>À propos de moi</div>
+      <div style={styles.div} onClick={() => onTabChange('contact')} className={activeTab === 'contact' ? 'active' : ''}>Me contacter</div>
+    </header>
+  );
+};
+
+const styles = {
+  div: {
+    flex: 1,
+    textAlign: 'center',
+    padding: '10px',
+    cursor: 'pointer',
+    border: '1px solid #ddd',
+    margin: '0 5px',
+    borderRadius: '4px',
+    transition: 'background-color 0.3s',
+  },
+};
 
 export default Header;
